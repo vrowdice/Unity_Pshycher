e6867e5f5400ab2849ce21db1c28d2028032e69e
Min Jea Kim <minmin310342@gmail.com>
2024-11-14 13:27:09 +0900

    new commit

---
+++ Assets/Scripts/Manager/TitleManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+    /// <summary>
+    /// 타이틀 패널
+    /// </summary>
+    [SerializeField]
+    GameObject m_titlePanel = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
